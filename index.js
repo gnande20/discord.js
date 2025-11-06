3dcabcf3b6ef3ebbc56494395eac2edf5bc3f3ae
gnande20 <gnandekouakou7@gmail.com>
2025-11-06 19:46:39 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-// Note : selon la version du fork, les importations peuvent varier — vérifie dans le code du repo.
+require("dotenv").config();
+const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ]
-})
-
-const token = process.env.BOT_TOKEN ||'MTQzNTczNDkzNDM0MDc2NzgzNQ.GJWN2B.U5ehVVjeXfzLfON64pnsCROHXH0shgPLlSdJuA';
-
-client.once('ready', () => {
-  console.log(`Connecté en tant que ${client.user.tag}`);
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
 });
 
-client.on('messageCreate', message => {
-  if (message.author.bot) return;
-  if (message.content === '!ping') {
-    message.reply('Pong !');
-  }
-});
+client.once("ready", () => console.log(`Connecté en tant que ${client.user.tag}`));
 
-client.login(token);
+client.login(process.env.TOKEN);
